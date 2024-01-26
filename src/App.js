34cf7d137df18254f9da0a531830101d4901f87e
samojeyinka <samuelojeyinka@gmail.com>
2024-01-26 01:26:09 -1200

    The nav done

--- src/App.js
+++ src/App.js
@@ -6,14 +6,17 @@ import { Routes,Route } from 'react-router-dom';
 
 function App() {
   return (
-    <>
+    <div className='app'>
+    <section className='navbar_section'>
     <Navbar/>
+    </section>
+    <section className='page_section'>
     <Routes>
-    
     <Route exact path='/' Component={Dashboard} />
     <Route path='/products' Component={Products} />
     </Routes>
-    </>
+    </section>
+    </div>
   );
   
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,60 +3,77 @@ import { NavLink } from 'react-router-dom';
 import { logo, nav1, nav2, nav3, nav4, nav5, nav6, nav7, nav8, nav9, moon, sun } from '../assets/assets'
 
 const Navbar = () => {
+
+
+  const handleNav = () => {
+    const linkTexts = document.querySelectorAll('.link_txt');
+    const expandIcon = document.getElementById('expand_icon');
+
+  linkTexts.forEach(linkText => {
+   if(linkText.style.maxWidth === '0px'){
+    linkText.style.maxWidth = 'fit-content';
+    expandIcon.style.rotate = '360deg'
+   } else{
+    linkText.style.maxWidth = '0px'
+    expandIcon.style.rotate = '180deg'
+   }
+  }
+)};
+
   return (
     <nav className='navbar'>
       <ul>
+      <span className='top_nav_box'>
         <span className='logo_box'>
           <NavLink to="/">
             <img src={logo} />
           </NavLink>
         </span>
 
-        <span className='middle_nav_box'>
           <li>
             <NavLink to="/">
               <img src={nav1} />
-              <span>Dashboard</span>
+              <span className='link_txt'>Dashboard</span>
             </NavLink>
           </li>
 
           <li>
             <NavLink to="/trending">
               <img src={nav2} />
-              <span>Trending</span>
+              <span className='link_txt'>Trending</span>
             </NavLink>
           </li>
 
           <li>
             <NavLink to="/users">
               <img src={nav3} />
-              <span>Users</span>
+              <span className='link_txt'>Users</span>
             </NavLink>
           </li>
 
           <li>
             <NavLink to="/store">
               <img src={nav4} />
-              <span>Store</span>
+              <span className='link_txt'>Store</span>
             </NavLink>
           </li>
 
           <li>
             <NavLink to="/updates">
               <img src={nav5} />
-              <span>Updates</span>
+              <span className='link_txt'>Updates</span>
             </NavLink>
           </li>
 
           <li>
             <NavLink to="/notifications">
               <img src={nav6} />
-              <span>Notifications</span>
+              <span className='link_txt'>Notifications</span>
             </NavLink>
           </li>
 
           <div className='switch_box'>
-            <div className='switch'>
+            <div className='switch_flex'>
               <img src={sun} />
               <img src={moon} />
             </div>
@@ -67,24 +84,23 @@ const Navbar = () => {
           <li>
             <NavLink to="/signout">
               <img src={nav7} />
-              <span>Sign out</span>
+              <span className='link_txt'>Sign out</span>
             </NavLink>
           </li>
 
           <li>
             <NavLink to="/settings">
               <img src={nav8} />
-              <span>Settings</span>
+              <span className='link_txt'>Settings</span>
             </NavLink>
           </li>
 
-          <li>
-            <img src={nav9} />
-            <span>Expand</span>
+          <li className='nav_handle' onClick={handleNav}>
+            <img src={nav9} id='expand_icon' />
+            <span className='link_txt'>Expand</span>
           </li>
 
         </span>
-
       </ul>
     </nav>
   )
