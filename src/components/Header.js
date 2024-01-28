aea4552a096326b2c956751ff2f664eeb76907f4
samojeyinka <samuelojeyinka@gmail.com>
2024-01-27 13:09:52 -1200

    added dynamic calendar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,35 @@
-import React from 'react'
+import React, { useState, useCallback } from 'react'
 import { FaSearch, FaCalendar, FaCalendarAlt, FaBell, FaLessThan, FaAngleDown } from 'react-icons/fa'
-import { BiCalendar, BiBell, BiCalendarAlt, BiX,BiCog,BiBarChart,BiBarChartAlt,BiBarChartAlt2,BiComment,BiPlus,BiLogOut,BiPowerOff } from 'react-icons/bi';
+import { BiCalendar, BiBell, BiCalendarAlt, BiX, BiCog, BiBarChart, BiBarChartAlt, BiBarChartAlt2, BiComment, BiPlus, BiLogOut, BiPowerOff } from 'react-icons/bi';
 import { user } from '../assets/assets'
 import '../stylesheets/Header.css'
 import { Link } from 'react-router-dom';
+import { Calendar } from '@natscale/react-calendar';
+import '@natscale/react-calendar/dist/main.css';
 
 const Header = () => {
 
+    const [value, setValue] = useState();
+
+    const onChange = useCallback(
+        (value) => {
+            setValue(value);
+        },
+        [setValue],
+    );
+
+    const handleCalendar = () => {
+        const showCalendar = document.getElementById('calendar');
+
+        const computedStyle = window.getComputedStyle(showCalendar);
+        const currentDisplay = computedStyle.getPropertyValue('display');
+
+        if (currentDisplay === 'none') {
+            showCalendar.style.display = 'block';
+        } else {
+            showCalendar.style.display = 'none';
+        }
+    };
     const showMenu = () => {
         const menu = document.getElementById("menuBox");
         menu.style.display = "inherit";
@@ -31,11 +54,15 @@ const Header = () => {
                         <input type='search' placeholder='Search...' />
                     </div>
 
-                    <div className='h-date'>
+                    <div className='h-date' onClick={handleCalendar}>
                         <i>
                             <BiCalendar />
                         </i>
                         <b>November 15,2023</b>
+                        <div className='calendar' id='calendar'>
+                            <Calendar value={value} onChange={onChange} color="green" />
+                        </div>
+
                     </div>
                     <div className='h-bell'>
                         <i>
@@ -64,66 +91,66 @@ const Header = () => {
                         <div className='mb-top'>
                             <div className='mbt-flex'>
                                 <button>Admin</button>
-                                <img src={user}/>
+                                <img src={user} />
                                 <h3>Justin Bergson</h3>
                             </div>
                         </div>
                         <div className='mb-md'>
                             <ul className='menu-links'>
                                 <div className='ml-top'>
-                                <li><Link>
-                                <i className='m-icon'>
-                                    <BiCog/>
-                                </i>
-                                <span>Account Settings</span>
-                                </Link></li>
-                                <li><Link>
-                                <i className='m-icon'>
-                                    <BiBarChart/>
-                                </i>
-                                <span>Project Usage</span>
-                                </Link></li>
-                                <li><Link>
-                                <i className='m-icon'>
-                                    <BiComment/>
-                                </i>
-                                <span>Support</span>
-                                </Link></li>
+                                    <li><Link>
+                                        <i className='m-icon'>
+                                            <BiCog />
+                                        </i>
+                                        <span>Account Settings</span>
+                                    </Link></li>
+                                    <li><Link>
+                                        <i className='m-icon'>
+                                            <BiBarChart />
+                                        </i>
+                                        <span>Project Usage</span>
+                                    </Link></li>
+                                    <li><Link>
+                                        <i className='m-icon'>
+                                            <BiComment />
+                                        </i>
+                                        <span>Support</span>
+                                    </Link></li>
                                 </div>
                                 <div className='ml-btm'>
-                                <li><Link>
-                                <i className='m-icon'>
-                                    <img src={user}/>
-                                </i>
-                                <span>Justin@gmail</span>
-                                </Link></li>
-                                <li><Link>
-                                <i className='m-icon'>
-                                    <BiPlus/>
-                                </i>
-                                <span>Create Team</span>
-                                </Link></li>
-                                
+                                    <li><Link>
+                                        <i className='m-icon'>
+                                            <img src={user} />
+                                        </i>
+                                        <span>Justin@gmail</span>
+                                    </Link></li>
+                                    <li><Link>
+                                        <i className='m-icon'>
+                                            <BiPlus />
+                                        </i>
+                                        <span>Create Team</span>
+                                    </Link></li>
+
                                 </div>
                             </ul>
                         </div>
                         <div className='mb-btm'>
-                        <li><Link onClick={hideMenu} >
+                            <li><Link onClick={hideMenu} >
                                 <i className='m-icon'>
-                                <BiX className='close-icon'/>
+                                    <BiX className='close-icon' />
                                 </i>
                                 <span>Close</span>
-                                </Link></li>
+                            </Link></li>
 
-                                <li className='logout'><Link>
+                            <li className='logout'><Link>
                                 <span>Logout</span>
                                 <i className='m-icon'>
-                                <BiPowerOff />
+                                    <BiPowerOff />
                                 </i>
-                                </Link></li>
+                            </Link></li>
 
                         </div>
-                    </div>  
+                    </div>
                 </div>
             </header>
         </>
